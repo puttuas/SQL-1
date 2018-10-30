b4f9bc211b6fe9565f5133dec6fd2fc8a0bde0f4
lahari1296 <42671433+lahari1296@users.noreply.github.com>
2018-10-30 12:13:50 +0530

    Update createtable.sql

--- createtable.sql
+++ createtable.sql
@@ -1,9 +1,8 @@
-CREATE TABLE Persons13
-(
-PersonID int,
-Name varchar(255),
-Address varchar(255),
-City varchar(255)
+CREATE TABLE Persons (
+    personID int NOT NULL PRIMARY KEY,
+    Name varchar(255) NOT NULL,
+    Address varchar(255),
+    City varchar(255)
 );
 
         
